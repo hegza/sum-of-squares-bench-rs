3086ccf2540abcc6695f67186807a5027684b5ec
Henri Lunnikivi <heggggxa@gmail.com>
2021-03-27 13:04:11 +0200

    Update doc

--- src/lib.rs
+++ src/lib.rs
@@ -6,12 +6,18 @@
     - H1.2 Data structures stored non-contiguently perform worse than those with contiguent storage. This is because the non-contiguent data structures cause more cache misses.
 - RQ2: Does the size of the data-structure affect the performance?
     - H2.1 The size of the array should have an approximately linear relationship with processing time with some discontinuity at thresholds corresponding to the various cache sizes on the computer under testing.
+- RQ3: Does it make a difference in measurement if the parameter is received by move vs. by reference.
+    - H3.1 It makes no difference since input values are only read. Care must be taken to make sure that dropping the input value is not added to measurement overhead.
 */
 
 use float_ord::FloatOrd;
 use std::iter;
 
-/// Sum the square of each input value
+/// Sum the square of each input value, taking ownership of the data-structure.
+///
+/// Takes ownership of a collection, transforms it into an iterator and maps
+/// over the iterator, squaring each input element. The subsequent iterator is
+/// then accumulated to a single 'sum' value.
 pub fn sum_of_squares<T>(collection: T) -> f64
 where
     T: iter::IntoIterator<Item = FloatOrd<f64>>,
